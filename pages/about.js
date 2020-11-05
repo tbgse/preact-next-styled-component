38f312693cb001d8a65ca4dcd36582bb37efdec4
Tobias Guse <tobe.guse@gmail.com>
2020-11-05 14:16:03 +0100

    add styled-components

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import styled from 'styled-components';
+
+const GreenDiv = styled.div`
+  width: 500px;
+  height: 500px;
+  background: green;
+`
 
 const About = () => (
   <main>
+    <GreenDiv />
     <h1>About us</h1>
   </main>
 );
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import Link from 'next/link'
+import styled from 'styled-components';
+
+
+const RedDiv = styled.div`
+  width: 500px;
+  height: 500px;
+  background: red;
+`
 
 const Home = () => {
   const [state, setState] = React.useState(0)
   return (
     <main>
+      <RedDiv />
       <h1>Hello from Preact</h1>
       <p>{state}</p>
       <button onClick={() => setState(state + 1)}>Increment</button>
